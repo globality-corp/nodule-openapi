b31de730aed35758a686f295efe74da21cae69a6
Sai Raveendran <sai.raveendran@globality.com>
2018-06-07 09:27:11 -0700

    Move paging logic into Nodule-OpenAPI
    * Add paging related error codes
    
    Why?
    
    It can be used indepdently of any GraphQL logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import Client from './client';
 
-export { OpenAPIError } from './error';
+export {
+    OpenAPIError,
+    MaxLimitReached,
+    TooManyResults,
+    NoResults,
+} from './error';
+export {
+    all,
+    any,
+    concurrentPaginate,
+    first,
+    none,
+    one,
+} from './modules';
 export default Client;
