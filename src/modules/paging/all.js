850431ee1652e8d7e3d52d663ff33425be4d4697
Yinon D. Nahamu <yinon.drornahamu@globality-contractor.com>
2019-12-29 14:35:37 +0200

    add unit test

--- src/modules/paging/all.js
+++ src/modules/paging/all.js
@@ -6,7 +6,17 @@ import concurrentPaginate from '../concurrency';
 const DEFAULT_LIMIT = 20;
 
 /**
- * Pagination for search requests that takes parameters in the body (for example in batch endpoints)
+ * Pagination for search requests that expect parameters to be passed in the body.
+ * For example:
+ * GET https://service.env.globality.io/api/resource/batch
+ * Content-Type: application/json; charset=UTF-8
+ * {
+ *     "identifiers": ["ID1", "ID2"... "ID1000"],
+ *     "limit": 20,
+ *     "offset": 0
+ * }
+ * A typical use case is for batch requests that send a long list of identifiers as a parameter
+ * In this case searching via URL query is not possible because of URL length limit
  * @body search requests args. Can optionally contain initial 'limit' and 'offset' values
  */
 export async function allForBodySearchRequest(
