f4a4340622ead2460f29837064902b552e36c36b
John Peterson <john.peterson@globality.com>
2023-08-18 11:50:05 +0100

    Adding ability to pass in specific headers into requests

---
+++ src/__tests__/operation.test.js
@@ -0,0 +1,63 @@
+import { extendHeadersFromOptions } from '../operation';
+
+describe('extendHeadersFromOptions function', () => {
+
+    it('should merge request.headers with options.additionalHeaders', () => {
+        const request = {
+            url: 'https://example.com/api',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer someToken',
+            },
+        };
+
+        const options = {
+            additionalHeaders: {
+                'X-Custom-Header': 'CustomValue',
+                'X-Another-Header': 'AnotherValue',
+            },
+        };
+
+        const expectedHeaders = {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer someToken',
+            'X-Custom-Header': 'CustomValue',
+            'X-Another-Header': 'AnotherValue',
+        };
+
+
+        const { headers } = extendHeadersFromOptions(request, options);
+        expect(headers).toEqual(expectedHeaders);
+    });
+
+    it('should return the same request if options.additionalHeaders does not exist', () => {
+        const request = {
+            url: 'https://example.com/api',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer someToken',
+            },
+        };
+
+        const options = {};
+
+        const result = extendHeadersFromOptions(request, options);
+        expect(result).toEqual(request);
+    });
+
+    it('should return the same request if options is not provided', () => {
+        const request = {
+            url: 'https://example.com/api',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer someToken',
+            },
+        };
+
+        const result = extendHeadersFromOptions(request);
+        expect(result).toEqual(request);
+    });
+});
---
+++ src/openApiCodeGenClients/__test__/helpers.test.js
@@ -0,0 +1,86 @@
+import { createHeaders } from '../helpers';
+
+
+jest.mock('@globality/nodule-config', () => ({
+    getMetadata: () => ({
+        name: 'my-service',
+    }),
+    getContainer: () => { },
+}));
+
+describe('createHeaders function', () => {
+    it('should merge initialHeaders with options.additionalHeaders', () => {
+        const startTime = new Date('2023-08-18T10:15:03.052Z');
+        const req = {
+            id: 'request-id',
+            locals: {
+                user: {
+                    id: 'user-id',
+                },
+            },
+            _startTime: startTime,
+        };
+
+        const context = {
+            spec: {},
+            path: '/my-api',
+            method: 'post',
+            options: {
+                retries: 5,
+            },
+        };
+        const options = {
+            additionalHeaders: {
+                'X-Request-My-Specific-Header': 'id-123',
+            },
+        };
+
+        const expectedFinalHeaders = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-Request-Service': 'my-service',
+            'X-Request-Started-At': '2023-08-18T10:15:03.052Z',
+            'X-Request-Id': 'request-id',
+            'X-Request-My-Specific-Header': 'id-123',
+            'X-Request-User': 'user-id',
+        };
+
+        const result = createHeaders(req, context, options);
+
+        expect(result).toEqual(expectedFinalHeaders);
+    });
+
+    it('should handle the absence of additionalHeaders property on options', () => {
+        const startTime = new Date('2023-08-18T10:15:03.052Z');
+        const req = {
+            id: 'request-id',
+            locals: {
+                user: {
+                    id: 'user-id',
+                },
+            },
+            _startTime: startTime,
+        };
+
+        const context = {
+            spec: {},
+            path: '/my-api',
+            method: 'post',
+            options: {
+                retries: 5,
+            },
+        };
+        const options = {}; // No additionalHeaders here
+
+        const expectedHeadersWithoutAdditionalHeaders = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-Request-Service': 'my-service',
+            'X-Request-Started-At': '2023-08-18T10:15:03.052Z',
+            'X-Request-Id': 'request-id',
+            'X-Request-User': 'user-id',
+        };
+
+        const result = createHeaders(req, context, options);
+
+        expect(result).toEqual(expectedHeadersWithoutAdditionalHeaders);
+    });
+});
